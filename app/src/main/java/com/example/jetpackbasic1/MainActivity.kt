1640eb3b8e2ebb6b2c6ed3238ee35fe2f68170ca
dqq <lezniak.daniel@gmail.com>
2024-07-03 15:40:53 +0200

    task 8

--- app/src/main/java/com/example/jetpackbasic1/MainActivity.kt
+++ app/src/main/java/com/example/jetpackbasic1/MainActivity.kt
@@ -4,22 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -37,21 +37,40 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
 @Composable
-fun MyApp(
-    modifier: Modifier = Modifier,
-    list: List<String> = listOf("Android", "Compose")
-) {
-    Surface() {
-        Column(modifier = modifier.padding(vertical = 5.dp)) {
-            list.forEach {
-                Greeting(name = it)
+private fun StartScreen(onButtonClick: () -> Unit) {
+    Surface(color = Color.White) {
+        Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+            Text(text = "Welcome to the Basics Codelab")
+            Button(onClick = onButtonClick ) {
+                Text(text = "Continue")
             }
         }
     }
 }
 
+@Composable
+fun MyApp(modifier: Modifier) {
+    var isBoardingMessageShowed by remember {
+        mutableStateOf(false)
+    }
+    if (isBoardingMessageShowed)
+        Greetings(modifier)
+    else
+        StartScreen(){
+            isBoardingMessageShowed = !isBoardingMessageShowed
+        }
+
+
+}
+@Composable
+private fun Greetings(modifier: Modifier = Modifier, list: List<String> = listOf("Android", "Compose")){
+    Column(modifier = modifier.padding(vertical = 5.dp)) {
+        list.forEach {
+            Greeting(name = it)
+        }
+    }
+}
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
@@ -61,7 +80,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 
     Surface(color = MaterialTheme.colorScheme.primary, modifier = Modifier.padding(vertical = 2.dp, horizontal = 5.dp)) {
         Row() {
-            Column(Modifier.weight(1f).padding(if (isExpanded.value) 50.dp else 0.dp)) {
+            Column(
+                Modifier
+                    .weight(1f)
+                    .padding(if (isExpanded.value) 50.dp else 0.dp)) {
                 Text(
                     text = "Hello"
                 )
@@ -87,5 +109,13 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     JetpackBasic1Theme {
         MyApp(modifier = Modifier.fillMaxSize())
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun StartScreenPrev() {
+    JetpackBasic1Theme {
+        StartScreen(  ) {}
     }
 }
