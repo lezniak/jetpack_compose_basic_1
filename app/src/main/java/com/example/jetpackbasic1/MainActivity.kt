ec7d40b241cee0eb11eb8a607c5e011738293be7
dqq <lezniak.daniel@gmail.com>
2024-07-03 16:30:25 +0200

    last task

--- app/src/main/java/com/example/jetpackbasic1/MainActivity.kt
+++ app/src/main/java/com/example/jetpackbasic1/MainActivity.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,8 +16,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -27,10 +37,16 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.coerceAtLeast
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackbasic1.ui.theme.JetpackBasic1Theme
 
 class MainActivity : ComponentActivity() {
@@ -64,7 +80,7 @@ private fun StartScreen(onButtonClick: () -> Unit) {
 @Composable
 fun MyApp(modifier: Modifier) {
     var isBoardingMessageShowed by rememberSaveable {
-        mutableStateOf(false)
+        mutableStateOf(true)
     }
     if (isBoardingMessageShowed)
         Greetings(modifier)
@@ -90,37 +106,40 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     var isExpanded by rememberSaveable {
         mutableStateOf(false)
     }
-    val extraPadding by animateDpAsState(
-        if (isExpanded) 48.dp else 0.dp,
-        animationSpec = spring(dampingRatio = Spring.DampingRatioMediumBouncy, stiffness = Spring.StiffnessLow)
-    )
-    Surface(
-        color = MaterialTheme.colorScheme.primary,
+
+    Card(
+        colors = CardDefaults.cardColors(),
+        shape = RoundedCornerShape(20.dp),
         modifier = Modifier.padding(vertical = 2.dp, horizontal = 5.dp)
     ) {
         Row() {
             Column(
                 Modifier
+                    .padding(16.dp)
                     .weight(1f)
-                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
+                    .animateContentSize(
+                        animationSpec = spring(
+                        dampingRatio = Spring.DampingRatioMediumBouncy,
+                        stiffness = Spring.StiffnessLow
+                    ))
             ) {
                 Text(
                     text = "Hello"
                 )
                 Text(
-                    text = "$name!"
+                    text = "$name",
+                    fontSize = 30.sp,
+                    fontWeight = FontWeight.Bold
                 )
+                if (isExpanded)
+                    Text(text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id posuere sem, sit amet porta ante. Nam pharetra est eu facilisis ornare. Duis nec mi eu ipsum porta tristique sit amet ac tortor. Nulla suscipit, ante eu porttitor posuere, mauris erat elementum mauris, ac tristique mauris enim varius tellus. Praesent tincidunt libero quis nibh pulvinar pellentesque. Donec et nibh sit amet erat gravida blandit at vitae nulla. Cras tempus consectetur nunc, a luctus mauris.")
             }
-            Button(
+            IconButton(
                 onClick = { isExpanded = !isExpanded },
                 modifier = Modifier
                     .weight(1f),
-                colors = ButtonDefaults.buttonColors(containerColor = Color.White)
             ) {
-                Text(
-                    if (isExpanded) "Show less" else "Show more",
-                    color = MaterialTheme.colorScheme.primary
-                )
+                Icon(imageVector = if (isExpanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore, contentDescription = "")
             }
         }
 
