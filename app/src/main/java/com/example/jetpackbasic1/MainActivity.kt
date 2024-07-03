42fa31552fe56be80aac1c13939821a3f037464e
dqq <lezniak.daniel@gmail.com>
2024-07-03 15:18:29 +0200

    task 6

--- app/src/main/java/com/example/jetpackbasic1/MainActivity.kt
+++ app/src/main/java/com/example/jetpackbasic1/MainActivity.kt
@@ -4,14 +4,22 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpackbasic1.ui.theme.JetpackBasic1Theme
@@ -29,19 +37,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(modifier: Modifier = Modifier) {
-    Surface(color = MaterialTheme.colorScheme.primary, modifier = modifier) {
-        Greeting(name = "Android")
+fun MyApp(
+    modifier: Modifier = Modifier,
+    list: List<String> = listOf("Android", "Compose")
+) {
+    Surface() {
+        Column(modifier = modifier.padding(vertical = 5.dp)) {
+            list.forEach {
+                Greeting(name = it)
+            }
+        }
     }
 }
 
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier.padding(24.dp)
-    )
+    Surface(color = MaterialTheme.colorScheme.primary, modifier = Modifier.padding(vertical = 2.dp, horizontal = 5.dp)) {
+        Row() {
+            Column(Modifier.weight(1f)) {
+                Text(
+                    text = "Hello"
+                )
+                Text(
+                    text = "$name!"
+                )
+            }
+            Button(
+                onClick = { } ,
+                modifier = Modifier
+                    .weight(1f),
+                colors = ButtonDefaults.buttonColors(containerColor = Color.White)
+            ) {
+                Text("Show more",color = MaterialTheme.colorScheme.primary)
+            }
+        }
+
+    }
 }
 
 @Preview(showBackground = true)
